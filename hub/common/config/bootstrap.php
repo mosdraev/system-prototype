dde363cbf5518344292f20c2ad0d4fbeda5963ec
CodeAvenue <dvdsn.12@gmail.com>
2018-06-13 01:35:12 +0800

    initial commit

---
+++ hub/common/config/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@website', dirname(dirname(__DIR__)) . '/website');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/admin');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@core', dirname(dirname(__DIR__)) . '/core');
